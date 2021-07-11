0ebb33b412702dcdf29aff09b995e908521a4db1
Sude Topal <sudeetpl@gmail.com>
2021-07-12 01:23:55 +0300

    Section 1:Generating Component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'movies', //<movies></movies>
+    templateUrl: 'movies.component.html'
+    // selector: '.movies' //<div class='movies'
+    // selector: '#movies' //<div id='movies'
+})
+export class MoviesComponent {
+
+}
