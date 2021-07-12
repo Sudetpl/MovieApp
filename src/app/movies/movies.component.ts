35b57b76091fcfb0fb2e0e50d17416c0c81b5e29
Sude Topal <sudeetpl@gmail.com>
2021-07-12 03:14:55 +0300

    Section 4: Templates

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,17 +1,26 @@
 import { style } from "@angular/animations";
 import { Component } from "@angular/core";
+import { Movie } from '../movie';
 
 
 @Component({
     selector: 'movies', //<movies></movies>
-    templateUrl: 'movies.component.html',
-    styles: [
-        'h2 { color:blue;}'
-        
-    ]
+    templateUrl: 'movies.component.html'
+    
+
+
     // selector: '.movies' //<div class='movies'
     // selector: '#movies' //<div id='movies'
 })
 export class MoviesComponent {
+     title = "Movie List";
 
+     movie: Movie = {
+         id: 1,
+         name: 'Movie Name'
+     }
+
+     getTitle(){
+         return this.title;
+     }
 }
