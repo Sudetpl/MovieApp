de0801ce25e9ecd01b113d893e268dce62d96f11
Sude Topal <sudeetpl@gmail.com>
2021-07-12 03:03:09 +0300

    Section 3: Component Styles

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,8 +1,14 @@
+import { style } from "@angular/animations";
 import { Component } from "@angular/core";
+
 
 @Component({
     selector: 'movies', //<movies></movies>
-    templateUrl: 'movies.component.html'
+    templateUrl: 'movies.component.html',
+    styles: [
+        'h2 { color:blue;}'
+        
+    ]
     // selector: '.movies' //<div class='movies'
     // selector: '#movies' //<div id='movies'
 })
