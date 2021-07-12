a939988779928a520ce565f30c4af452b62c88b6
sude <sudeetpl@gmail.com>
2021-07-12 15:09:28 +0300

    Section 6:Event-Binding

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,18 +1,21 @@
 import { style } from "@angular/animations";
 import { Component } from "@angular/core";
+import { Movie } from "../movie";
 import { Movies } from '../movie.datasource';
 
 @Component({
     selector: 'movies', //<movies></movies>
     templateUrl: 'movies.component.html'
-    
-
-
     // selector: '.movies' //<div class='movies'
     // selector: '#movies' //<div id='movies'
 })
 export class MoviesComponent {
      title = "Movie List";
      movies = Movies;
+     selectedMovie!: Movie;
+
+     onSelect(movie:Movie):void{
+               this.selectedMovie = movie;
+     }
      
 }
