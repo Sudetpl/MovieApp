61ac97604295a20781d8d687897f2f80d643ffcc
sude <sudeetpl@gmail.com>
2021-07-13 15:38:51 +0300

    Section-14-Adding-New-Movie

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,7 +1,7 @@
 import { style } from "@angular/animations";
 import { Component } from "@angular/core";
 import { Movie } from "../movie";
-import { MovieService} from '../movie.service';
+import { MovieService } from '../movie.service';
 
 
 @Component({
@@ -11,26 +11,39 @@ import { MovieService} from '../movie.service';
     // selector: '#movies' //<div id='movies'
 })
 export class MoviesComponent {
-     title = "Movie List";
-     movies!: Movie[];
-     selectedMovie!: Movie;
+    title = "Movie List";
+    movies!: Movie[];
+    selectedMovie!: Movie;
 
-     constructor(private movieService:MovieService){}
+    constructor(private movieService: MovieService) { }
 
-     ngOnInit(): void {
-         this.getMovies();
-         
-     }
+    ngOnInit(): void {
+        this.getMovies();
 
-     onSelect(movie:Movie):void{
-            this.selectedMovie = movie;
-     }
+    }
 
-     getMovies():void {
-          this.movieService.getMovies()
-                        .subscribe(movies=>{
-                            this.movies=movies;
-                        });
-     }
-     
+    onSelect(movie: Movie): void {
+        this.selectedMovie = movie;
+    }
+
+    getMovies(): void {
+        this.movieService.getMovies()
+            .subscribe(movies => {
+                this.movies = movies;
+            });
+    }
+
+    add(name: string, imageUrl: string, description: string): void {
+        this.movieService.add({
+            name,
+            imageUrl,
+            description
+        } as Movie).subscribe(movie => this.movies.push(movie));
+    }
+
+    delete(movie: Movie): void {
+        this.movies = this.movies.filter(m => m !== movie);
+        this.movieService.delete(movie).subscribe();
+    }
+
 }
